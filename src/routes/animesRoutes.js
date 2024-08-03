d34cfec24ad15c890ec9e03d31db106ecb495a30
Irene García <irenegwodak@gmail.com>
2024-08-03 10:31:33 +0200

    modularize routes - move all anime routes

--- src/routes/animesRoutes.js
+++ src/routes/animesRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { getDatabaseConnection } = require('../config/db');
 
+const { getDatabaseConnection } = require('../config/db');
+const { generateToken, authenticateToken } = require('../utils/tokenUtils');
 
 const maxYear = new Date().getFullYear() + 2;
 
@@ -30,5 +31,272 @@ router.get('/animes', async (req, res) => {
   }
 });
 
+router.get('/animes/:id', async (req, res) => {
+  const ID = req.params.id;
+
+  if (isNaN(parseInt(ID))) {
+    // if (!validate.id(ID)) {
+    return res.status(400).json({
+      success: false,
+      error: 'id must be a number',
+    });
+  }
+
+  const querySelectAnimeById = `SELECT * FROM animes WHERE idAnime = ?;`;
+
+  console.log('Querying database');
+  try {
+    const conn = await getDatabaseConnection();
+    const [animes] = await conn.query(querySelectAnimeById, [ID]);
+    const anime = animes[0];
+    conn.end();
+    if (animes.length === 0) {
+      return res.status(404).json({
+        success: false,
+        error: 'anime not found',
+      });
+    }
+    res.json({
+      success: true,
+      results: anime,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      error: 'database error',
+    });
+  }
+});
+
+router.post('/animes', authenticateToken, async (req, res) => {
+  const { title, year, chapters } = req.body;
+
+  // INPUT VALIDATION
+
+  if (!title || !year || !chapters) {
+    return res.status(400).json({
+      success: false,
+      error: 'title, year and chapters are required fields',
+    });
+  }
+  if (typeof title !== 'string') {
+    return res.status(400).json({
+      success: false,
+      error: 'title must be text',
+    });
+  }
+  if (isNaN(parseInt(year)) || isNaN(parseInt(chapters))) {
+    return res.status(400).json({
+      success: false,
+      error: 'year and chapters must be numbers',
+    });
+  }
+  if (!(1900 < parseInt(year) && parseInt(year) < maxYear)) {
+    return res.status(400).json({
+      success: false,
+      error: `year must be after 1900 and before ${maxYear}`,
+    });
+  }
+
+  const MAX_ANIME_COUNT = 11;
+  const queryCountAllAnimes = 'SELECT COUNT(*) AS total_of_animes FROM animes;';
+  const querySelectByTitle = 'SELECT * FROM animes WHERE title = ?';
+  const queryInsertAnime =
+    'INSERT INTO animes (title, year, chapters) VALUES (?, ?, ?);';
+
+  console.log('Sending data to database');
+  try {
+    const conn = await getDatabaseConnection();
+
+    // Check total animes in db
+    const [[{ total_of_animes }]] = await conn.query(queryCountAllAnimes);
+
+    if (total_of_animes >= MAX_ANIME_COUNT) {
+      conn.end();
+      console.log('Conection ended');
+
+      return res.status(400).json({
+        success: false,
+        error: 'The maximum number of anime to register has been reached',
+      });
+    }
+
+    // Check if title already exists
+    const [animeByTitle] = await conn.query(querySelectByTitle, [title]);
+    if (animeByTitle.length !== 0) {
+      conn.end();
+      console.log('Conection ended');
+
+      return res.status(400).json({
+        success: false,
+        error: 'This title already exists',
+      });
+    }
+
+    // Insert new data
+    const [resultAnime] = await conn.query(queryInsertAnime, [
+      title,
+      year,
+      chapters,
+    ]);
+
+    conn.end();
+    console.log('Conection ended');
+
+    return res.status(200).json({
+      success: true,
+      message: 'Anime created',
+      idNewAnime: resultAnime.insertId,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      success: false,
+      error: 'database error',
+    });
+  }
+});
+
+/* Example
+  ​http://localhost:3113/animes/4
+  {
+  "title": "Kaze no Stigma",
+  "year": "2023",
+  "chapters": "10"
+} */
+router.put('/animes/:animeId', authenticateToken, async (req, res) => {
+  const paramsId = req.params.animeId;
+  const { title, year, chapters } = req.body;
+
+  //input validation
+  if (isNaN(parseInt(paramsId))) {
+    return res.status(400).json({
+      success: false,
+      error: 'id must be a number',
+    });
+  }
+  if (!title || !year || !chapters) {
+    return res.status(400).json({
+      success: false,
+      error: 'title, year and chapters are required fields',
+    });
+  }
+  if (typeof title !== 'string') {
+    return res.status(400).json({
+      success: false,
+      error: 'title must be text',
+    });
+  }
+  if (isNaN(parseInt(year)) || isNaN(parseInt(chapters))) {
+    return res.status(400).json({
+      success: false,
+      error: 'year and chapters must be numbers',
+    });
+  }
+  if (!(1900 < parseInt(year) && parseInt(year) < maxYear)) {
+    return res.status(400).json({
+      success: false,
+      error: `year must be after 1900 and before ${maxYear}`,
+    });
+  }
+
+  //Check if anime exists in db by id
+  console.log('Checking if id exists');
+
+  const queryIfIdExists = `SELECT * FROM animes WHERE idAnime = ?;`;
+  const queryIfTitleExists = `SELECT * FROM animes WHERE title = ?;`;
+  const queryToModifyAnime =
+    'UPDATE animes SET title = ?, year = ?, chapters = ? WHERE idAnime = ?;';
+  try {
+    const conn = await getDatabaseConnection();
+    // Get data to check if id/title exists and to send in response
+    const [animesIdSearch] = await conn.query(queryIfIdExists, [paramsId]);
+    //doesnt exist:
+    if (animesIdSearch.length === 0) {
+      conn.end();
+      return res.status(404).json({
+        success: false,
+        error: 'anime not found',
+      });
+    }
+
+    const [animeTitleSearch] = await conn.query(queryIfTitleExists, [title]);
+    //doesnt exist:
+    if (animeTitleSearch.length !== 0) {
+      conn.end();
+      return res.status(400).json({
+        success: false,
+        error: 'this title already exists',
+      });
+    }
+
+    //Exists: send data to modify in db
+    console.log('Sending data to database');
+    const [modifyAnime] = await conn.query(queryToModifyAnime, [
+      title,
+      year,
+      chapters,
+      paramsId,
+    ]);
+    //Get new data to send in response
+    const [animesModified] = await conn.query(queryIfIdExists, [paramsId]);
+    conn.end();
+
+    res.status(200).json({
+      success: true,
+      msg: 'data modified successfully',
+      'previous data': animesIdSearch[0],
+      'new data': animesModified[0],
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      error: 'database error',
+    });
+  }
+});
+
+/* Example 
+  ​http://localhost:3113/animes/14
+*/
+router.delete('/animes/:animeId', authenticateToken, async (req, res) => {
+  console.log('Querying database');
+  const paramsId = req.params.animeId;
+  if (isNaN(parseInt(paramsId))) {
+    return res.status(400).json({
+      success: false,
+      error: 'id must be a number',
+    });
+  }
+
+  const queryIfAnimeExists = `SELECT * FROM animes WHERE idAnime = ?;`;
+  try {
+    const conn = await getDatabaseConnection();
+    const [animes] = await conn.query(queryIfAnimeExists, [paramsId]);
+    if (animes.length === 0) {
+      conn.end();
+      return res.status(404).json({
+        success: false,
+        error: 'anime not found',
+      });
+    }
+    const queryDeleteAnime = 'DELETE FROM animes WHERE idAnime = ?;';
+    const [animeDeleted] = await conn.query(queryDeleteAnime, [paramsId]);
+    conn.end();
+
+    res.status(200).json({
+      success: true,
+      msg: `anime "${animes[0].title}" deleted successfully`,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      error: 'database error',
+    });
+  }
+});
 
 module.exports = router;
