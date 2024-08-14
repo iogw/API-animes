1a0c0e6c50a51f8656ec34f6bae9d67182bdd212
Irene García <irenegwodak@gmail.com>
2024-08-14 15:02:34 +0200

    authentication middleware refactor with module apiResponse

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,14 +1,13 @@
 const { verify } = require('../utils/tokenUtils');
+const { jsonRes, MSG } = require('../utils/apiResponse');
 
 const authenticate = (req, res, next) => {
   const token = req.headers['authorization'];
-  if (!token) {
-    return res.status(401).json({ error: 'Token not provided' });
-  }
   const decoded = verify(token);
-  if (!decoded) {
-    return res.status(401).json({ error: 'Invalid token' });
-  }
+
+  if (!token)
+    return jsonRes(res, 'forbidden', { error: MSG.TOKEN_NOT_PROVIDED });
+  if (!decoded) return jsonRes(res, 'forbidden', { error: MSG.INVALID_TOKEN });
   req.user = decoded;
   next();
 };
